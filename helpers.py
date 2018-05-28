52a902d75bdf6375cc5d5eb71a0c278085098fcd
Pierre Gabioud <p.gabioud@epfl.ch>
2018-05-28 19:48:34 +0200

    changed name raised_cosine

--- helpers.py
+++ helpers.py
@@ -1,18 +1,23 @@
 import numpy as np
 import scipy.signal as sgn
 
-def raised_cosine(freq, pulse_dt, rate, beta=0.5):
-    freq /=2
+def raised_cosine(shifted_freq, time_per_symb, samp_freq, roll_off=0.5):
+    shifted_freq /= 2
     np.seterr(invalid='ignore')
-    pulse_t = np.linspace(- pulse_dt, pulse_dt , rate * pulse_dt)
-    p_const = 4 * beta / (np.pi * np.sqrt(pulse_dt))
-    p_cos = np.cos((1 + beta) * np.pi * pulse_t / pulse_dt) 
-    p_sinc = (1 - beta) * np.pi / (4 * beta) * np.sinc((1 - beta) * pulse_t / pulse_dt)
-    p_denomin = 1 - (4 * beta * pulse_t / pulse_dt) ** 2
-    p_hosp = beta / (np.pi * np.sqrt(2 * pulse_dt)) * ((np.pi + 2) * np.sin(np.pi / (4 * beta)) + (np.pi - 2) * np.cos(np.pi / (4 * beta)))
-    out = p_const * (p_cos + p_sinc) / p_denomin
-    out[np.isnan(out)] = p_hosp
-    out *= np.cos(2 * np.pi * freq * pulse_t)
+    
+    pulse = np.linspace(- time_per_symb, time_per_symb, samp_freq * time_per_symb)
+    
+    numerator = np.cos((1 + roll_off) * np.pi * pulse / time_per_symb) 
+              + (1 - roll_off) * np.pi / (4 * roll_off) * np.sinc((1 - roll_off) * pulse / time_per_symb)
+    denominator = 1 - (4 * roll_off * pulse / time_per_symb) ** 2
+    
+    limit_case = roll_off / (np.pi * np.sqrt(2 * time_per_symb)) * ((np.pi + 2) * np.sin(np.pi / (4 * roll_off)) 
+             + (np.pi - 2) * np.cos(np.pi / (4 * roll_off)))
+    
+    out = 4 * roll_off / (np.pi * np.sqrt(time_per_symb)) * numerator / denominator
+    out[np.isnan(out)] = limit_case
+    out *= np.cos(2 * np.pi * shifted_freq * pulse)
+    
     assert not any(np.isnan(out))    
     np.seterr(invalid='warn')
     return out
